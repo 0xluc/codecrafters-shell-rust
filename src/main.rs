22288ef67286fa916458f08e3f89eae126c6c739
Lucas <lucasbrodrigues14@gmail.com>
2025-02-20 09:42:34 +0000

    implements the exit command

--- src/main.rs
+++ src/main.rs
@@ -9,9 +9,12 @@ fn main() {
         let stdin = io::stdin();
         let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
-        println!(
-            "{}: command not found",
-            input[0..input.len() - 1].to_string()
-        );
+        let command = input[0..input.len() - 1].to_string();
+        let command_tokens: Vec<&str> = command.split_whitespace().collect();
+        if command_tokens[0] == "exit" {
+            std::process::exit(command_tokens[1].parse::<i32>().unwrap());
+        }
+
+        println!("{}: command not found", command);
     }
 }
