1a61cf1d68b614f1e261ff7523880ab5ad786f82
Lucas <lucasbrodrigues14@gmail.com>
2025-02-21 04:42:21 +0000

    implements the type command

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@
 use std::io::{self, Write};
 
 fn main() {
+    let methods = ["exit", "echo", "type"];
     loop {
         print!("$ ");
         io::stdout().flush().unwrap();
@@ -11,20 +12,31 @@ fn main() {
         stdin.read_line(&mut input).unwrap();
         let command = input[0..input.len() - 1].to_string();
         let command_tokens: Vec<&str> = command.split_whitespace().collect();
-        if command_tokens[0] == "exit" {
-            std::process::exit(command_tokens[1].parse::<i32>().unwrap());
-        }
-
-        if command_tokens[0] == "echo" {
-            if command_tokens.len() < 2 {
-                println!("");
-                continue;
+        match command_tokens[0] {
+            "exit" => {
+                std::process::exit(command_tokens[1].parse::<i32>().unwrap());
             }
-            let no_echo = &command_tokens[1..command_tokens.len()];
-            println!("{}", no_echo.join(" "));
-            continue;
+            "echo" => {
+                if command_tokens.len() < 2 {
+                    println!("");
+                } else {
+                    println!("{}", &command_tokens[1..command_tokens.len()].join(" "));
+                }
+            }
+            "type" => {
+                let no_type_string = &command_tokens[1..command_tokens.len()];
+                if methods
+                    .iter()
+                    .any(|&method| method == &no_type_string.join(" "))
+                {
+                    println!("{} is a shell builtin", no_type_string.join(" "));
+                } else {
+                    println!("{}: not found", no_type_string.join(" "));
+                }
+            }
+            _ => {
+                println!("{}: command not found", command);
+            }
         }
-
-        println!("{}: command not found", command);
     }
 }
