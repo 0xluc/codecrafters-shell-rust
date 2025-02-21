8496ef9691452458261fe8877900657ffd4c9ffd
Lucas <lucasbrodrigues14@gmail.com>
2025-02-21 09:10:47 +0000

    extends type to search for executable files using PATH

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
+use std::env;
+use std::fs;
 #[allow(unused_imports)]
 use std::io::{self, Write};
 
 fn main() {
-    let methods = ["exit", "echo", "type"];
-    loop {
+    let mut paths = String::from("");
+    match env::var("PATH") {
+        Ok(v) => paths = v,
+        Err(e) => println!("{}", e),
+    }
+    let path_vec: Vec<&str> = paths.split(":").collect();
+
+    'outer: loop {
         print!("$ ");
         io::stdout().flush().unwrap();
 
@@ -24,15 +32,17 @@ fn main() {
                 }
             }
             "type" => {
-                let no_type_string = &command_tokens[1..command_tokens.len()];
-                if methods
-                    .iter()
-                    .any(|&method| method == &no_type_string.join(" "))
-                {
-                    println!("{} is a shell builtin", no_type_string.join(" "));
-                } else {
-                    println!("{}: not found", no_type_string.join(" "));
+                let no_type_string = &command_tokens[1..command_tokens.len()].join(" ");
+                for path in &path_vec {
+                    match fs::metadata(format!("{path}/{no_type_string}")) {
+                        Ok(_) => {
+                            println!("{} is {}", no_type_string, path);
+                            continue 'outer;
+                        }
+                        Err(_) => {}
+                    }
                 }
+                println!("{}: not found", no_type_string);
             }
             _ => {
                 println!("{}: command not found", command);
